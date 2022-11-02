769e88703713bf23ecac5e52afc65a4828b18b59
isabellemsan <isabelle.muresan@cloudreach.com>
2022-11-02 17:00:22 +0100

    created logstash server

--- logstash.tf
+++ logstash.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "logstash" {
   ami                    = data.aws_ami.aws_ubuntu_image.id
   instance_type          = var.logstash_instance_type
-  subnet_id              = aws_subnet.private_subnet_name.id
+  subnet_id              = data.aws_subnet.subnet_g_ls_e.id
   vpc_security_group_ids = [aws_security_group.logstash_sg.id]
   key_name               = var.keypair_name
+  iam_instance_profile   = "instance-profile"
 
 
   tags = {
@@ -11,22 +12,33 @@ resource "aws_instance" "logstash" {
   }
 
 }
-
+resource "aws_volume_attachment" "logstash_ebs_attach" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ebs_logstash.id
+  instance_id = aws_instance.logstash.id
+}
 resource "aws_security_group" "logstash_sg" {
-    name="logstash_sg"
-    descritption= "SG for the Logtstash server"
-    vpc_id = data.aws_vpc.main.id
-}
+  name         = "logstash_sg"
+  description = "SG for the Logtstash server"
+  vpc_id       = data.aws_vpc.main.id
 
-#Inbound connection
-ingress{
-    description="beats port"
-    from_port= 5503
-    to_port=5503
-    protocol="tcp"
-    cidr_blocks=["0.0.0.0/0"]
-}
-    # 
+
+  #Inbound connection
+  ingress {
+    description = "beats port"
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "web api call"
+    from_port   = 9600
+    to_port     = 9700
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # 
   egress {
     description = "Allow access to the world"
     from_port   = 0
@@ -36,4 +48,4 @@ ingress{
   }
 
 
-
+}
